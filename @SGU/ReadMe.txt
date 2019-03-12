1e3581e11cf1e3b53c599e770a4c94630f4e62d5
Trox <26691952+Rille220@users.noreply.github.com>
2019-03-12 21:07:01 +0100

    SGU Mod
    My mod. Contains some vests and backpacks.

---
+++ @SGU/ReadMe.txt
@@ -0,0 +1,12 @@
+This mod is specially made for the group S.G.U (Swedish Gaming Unit)
+
+
+Thanks to SAM3 Swedish Army Mod 3 for their great mod. Their gray vest is the base of the vest textures.
+
+Thanks to Astartes Gaming for his great tutorials on youtube on how to edit textures in arma. check his youtube out below.
+https://www.youtube.com/channel/UCDO91SuZpz9lpK0O8xXWnyw
+
+Please do NOT redistrubute this mod.
+
+Thanks
+Trox S.G.U
---
+++ @SGU/mod.cpp
@@ -0,0 +1,13 @@
+name = "S.G.U";
+picture = "SGU_Patch_x512_512.paa";
+logoSmall = "SGU_Patch_x64_64.paa";
+logo = "SGU_Patch_x64_64.paa";
+logoOver = "SGU_Patch_x64_64.paa";
+action = "http://www.swedish-gaming-unit.se/";
+actionName = "Website";
+tooltip = "S.G.U";
+tooltipOwned = "S.G.U";
+dlcColor[] = {1, 0.76, 0, 1};
+overview = "<a href='http://www.swedish-gaming-unit.se/'>Swedish-Gaming-Unit</a>, SGU contains custom items and improvements for the community group SGU. <br/><br/>Created by Trox.";
+hideName = 0;
+hidePicture = 0;
