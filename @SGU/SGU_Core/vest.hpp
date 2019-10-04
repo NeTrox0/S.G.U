4e730936e6f3690c9103589c8a8163d569d27d64
Trox <26691952+Rille220@users.noreply.github.com>
2019-10-04 21:47:31 +0200

    Addition of tan vests

---
+++ @SGU/SGU_Core/vest.hpp
@@ -0,0 +1,205 @@
+/*
+	Config for Vests
+	
+	By Trox
+*/	
+	class SGU_Carrier_Rig: Vest_Camo_Base
+    {
+		author = "Trox";
+		_generalMacro="SGU_Carrier_Rig";
+        scope = 2;
+        displayName = "SGU Carrier Rig";
+        picture = "\A3\Characters_f\Data\UI\icon_V_plate_carrier_2_CA.paa";
+        model = "\A3\Characters_F\BLUFOR\equip_b_Vest01.p3d";
+        hiddenSelections[] = {"camo"};
+        hiddenSelectionsTextures[] = {"\SGU_Core\Data\vest\SGU_vest_co.paa"};
+		descriptionShort="$STR_A3_SP_AL_IV";
+        class ItemInfo: VestItem
+        { 
+            uniformModel = "\A3\Characters_F\BLUFOR\equip_b_Vest01.p3d";
+            hiddenSelections[] = {"camo"};
+			containerClass = "Supply160";
+			mass = 80;
+			class HitpointsProtectionInfo //more info at: https://community.bistudio.com/wiki/Arma_3_Soldier_Protection
+            {
+                class Chest
+				{
+					HitpointName="HitChest";
+					armor=24;
+					PassThrough=0.14;
+				};
+				class Diaphragm
+				{
+					HitpointName="HitDiaphragm";
+					armor=24;
+					PassThrough=0.14;
+				};
+				class Abdomen
+				{
+					hitpointName="HitAbdomen";
+					armor=24;
+					passThrough=0.14;
+				};
+                class Body
+                {
+                    hitPointName="HitBody";
+                    passThrough = 0.16;
+                };
+            };
+        }; 
+    };
+    class SGU_Carrier_Rig_Lite: Vest_Camo_Base
+    {
+		author = "Trox";
+		_generalMacro="SGU_Carrier_Rig_Lite";
+        scope = 2;
+        displayName = "SGU Carrier Rig Lite";
+        picture = "\A3\Characters_f\Data\UI\icon_V_plate_carrier_1_CA.paa";
+        model = "\A3\Characters_F\BLUFOR\equip_b_Vest02.p3d";
+        hiddenSelections[] = {"camo"};
+        hiddenSelectionsTextures[] = {"\SGU_Core\Data\vest\SGU_vest_co.paa"};
+		descriptionShort="$STR_A3_SP_AL_III";
+        class ItemInfo: VestItem
+        {
+            uniformModel = "\A3\Characters_F\BLUFOR\equip_b_Vest02.p3d";
+            hiddenSelections[] = {"camo"};
+			containerClass = "Supply160";
+			mass = 60;
+			class HitpointsProtectionInfo
+            {
+                class Chest
+				{
+					HitpointName="HitChest";
+					armor=22;
+					PassThrough=0.16;
+				};
+				class Diaphragm
+				{
+					HitpointName="HitDiaphragm";
+					armor=22;
+					PassThrough=0.16;
+				};
+				class Abdomen
+				{
+					hitpointName="HitAbdomen";
+					armor=22;
+					passThrough=0.16;
+				};
+                class Body
+                {
+                    hitPointName="HitBody";
+                    passThrough = 0.16;
+                };
+            };
+        }; 
+    };
+    class SGU_Carrier_Rig_Name: V_PlateCarrier_Kerry
+    {
+		author = "Trox";
+		_generalMacro="SGU_Carrier_Rig_Name";
+        scope = 2;
+        displayName = "SGU Carrier Rig Name";
+        picture = "\A3\Characters_f\Data\UI\icon_V_plate_carrier_1_CA.paa";
+        model = "\A3\Characters_F_EPA\BLUFOR\equip_b_vest_kerry.p3d";
+        hiddenSelections[] = {"camo"};
+        hiddenSelectionsTextures[] = {"\SGU_Core\Data\vest\SGU_vest_co.paa"};
+		descriptionShort="$STR_A3_SP_AL_III";
+		class ItemInfo: VestItem
+        { 
+            uniformModel = "\A3\Characters_F_EPA\BLUFOR\equip_b_vest_kerry.p3d";
+            hiddenSelections[] = {"camo"};
+			containerClass = "Supply160";
+			mass = 60;
+			class HitpointsProtectionInfo
+            {
+				class Chest
+				{
+					HitpointName="HitChest";
+					armor=22;
+					PassThrough=0.16;
+				};
+				class Diaphragm
+				{
+					HitpointName="HitDiaphragm";
+					armor=22;
+					PassThrough=0.16;
+				};
+				class Abdomen
+				{
+					hitpointName="HitAbdomen";
+					armor=22;
+					passThrough=0.16;
+				};
+                class Body
+                {
+                    hitPointName="HitBody";
+                    passThrough = 0.16;
+                };
+            };
+        }; 
+    };
+	class SGU_Carrier_Rig_Trox: V_PlateCarrier_Kerry
+    {
+		author = "Trox";
+		_generalMacro="SGU_Carrier_Rig_Name";
+        scope = 2;
+        displayName = "SGU Carrier Rig Trox";
+        picture = "\A3\Characters_f\Data\UI\icon_V_plate_carrier_1_CA.paa";
+        model = "\A3\Characters_F_EPA\BLUFOR\equip_b_vest_kerry.p3d";
+        hiddenSelections[] = {"camo"};
+        hiddenSelectionsTextures[] = {"\SGU_Core\Data\vest\SGU_vest_trox_co.paa"};
+		descriptionShort="$STR_A3_SP_AL_III";
+		class ItemInfo: VestItem
+        { 
+            uniformModel = "\A3\Characters_F_EPA\BLUFOR\equip_b_vest_kerry.p3d";
+            hiddenSelections[] = {"camo"};
+			containerClass = "Supply160";
+			mass = 60;
+			class HitpointsProtectionInfo
+            {
+				class Chest
+				{
+					HitpointName="HitChest";
+					armor=22;
+					PassThrough=0.13;
+				};
+				class Diaphragm
+				{
+					HitpointName="HitDiaphragm";
+					armor=22;
+					PassThrough=0.13;
+				};
+				class Abdomen
+				{
+					hitpointName="HitAbdomen";
+					armor=22;
+					passThrough=0.13;
+				};
+                class Body
+                {
+                    hitPointName="HitBody";
+                    passThrough = 0.16;
+                };
+            };
+        }; 
+    };
+	
+	//Tan vest colour
+	class SGU_Carrier_Rig_Tan: SGU_Carrier_Rig
+    {
+		author = "Trox";
+        displayName = "SGU Carrier Rig Tan";
+        hiddenSelectionsTextures[] = {"\SGU_Core\Data\vest\SGU_vest_tan_co.paa"};
+    };
+    class SGU_Carrier_Rig_Lite_Tan: SGU_Carrier_Rig_Lite
+    {
+		author = "Trox";
+        displayName = "SGU Carrier Rig Lite Tan";
+        hiddenSelectionsTextures[] = {"\SGU_Core\Data\vest\SGU_vest_tan_co.paa"};
+    };
+    class SGU_Carrier_Rig_Name_Tan: SGU_Carrier_Rig_Name
+    {
+		author = "Trox";
+        displayName = "SGU Carrier Rig Tan Name";
+        hiddenSelectionsTextures[] = {"\SGU_Core\Data\vest\SGU_vest_tan_co.paa"};
+    };
